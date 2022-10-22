e36a48a02f0bb885d414e5b23a9a17e432281f07
fidus-hub <medaminegrissen@gmail.com>
2022-10-22 09:40:12 +0100

    cnst it

--- includes/random_access_iterator.hpp
+++ includes/random_access_iterator.hpp
@@ -2,22 +2,30 @@
 #pragma once
 
 #include <iostream>
+#include "utils.hpp"
 
 namespace ft
 {
-     template <class T, class Alloc = std::allocator<T> > 
-    class random_access_iterator
-    {
-        public :
-		typedef T																						value_type;
-		typedef typename std::iterator<std::random_access_iterator_tag, value_type>::pointer			pointer;
-		typedef typename std::iterator<std::random_access_iterator_tag, value_type>::reference			reference;
-		typedef typename std::iterator<std::random_access_iterator_tag, value_type>::difference_type	difference_type;
-		typedef typename std::iterator<std::random_access_iterator_tag, value_type>::iterator_category	iterator_category;
+	template <class T> 
+	class random_access_iterator
+	{
+		public :
+			typedef T																						value_type;
+			typedef T*																						pointer;
+			typedef T&																						reference;
+			typedef typename ft::iterator_traits<std::random_access_iterator_tag, T>::difference_type		difference_type;
+			typedef typename ft::iterator_traits<std::random_access_iterator_tag, T>::iterator_category		iterator_category;
+
+			random_access_iterator() :itr_ptr(nullptr) {}
+			random_access_iterator(pointer ptr) : itr_ptr(ptr) {}
+			random_access_iterator(const random_access_iterator& copy) : itr_ptr(copy.itr_ptr) {}
+			~random_access_iterator() {}
+
+
 
 
 		protected:
-			pointer _iterator; 
+			pointer itr_ptr; 
 	}
 
 }

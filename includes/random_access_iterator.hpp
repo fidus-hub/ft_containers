c5d6be6b3ad613cc5fb5c3aa7ab5cffb2fe9e7fe
fidus-hub <medaminegrissen@gmail.com>
2022-10-22 11:42:41 +0100

    oprt

--- includes/random_access_iterator.hpp
+++ includes/random_access_iterator.hpp
@@ -18,14 +18,79 @@ namespace ft
 
 			random_access_iterator() :itr_ptr(nullptr) {}
 			random_access_iterator(pointer ptr) : itr_ptr(ptr) {}
-			random_access_iterator(const random_access_iterator& copy) : itr_ptr(copy.itr_ptr) {}
+			random_access_iterator(const random_access_iterator& other) : itr_ptr(other.itr_ptr) {}
 			~random_access_iterator() {}
 
+			random_access_iterator& operator=(const random_access_iterator& other)
+			{
+				itr_ptr = other.itr_ptr;
+				return *this;
+			}
 
+			pointer	get_pointer() const
+			{
+				return (itr_ptr);
+			}
 
+			random_access_iterator& operator++()
+			{
+				itr_ptr++;
+				return *this;
+			}
+			random_access_iterator& operator++(int)
+			{
+				random_access_iterator tmp(itr_ptr);
+				itr_ptr++;
+				return tmp;
+			}
+			random_access_iterator& operator--()
+			{
+				itr_ptr--;
+				return *this;
+			}
+			random_access_iterator& operator--(int)
+			{
+				random_access_iterator tmp(itr_ptr);
+				operator--();
+				return tmp;
+			}
+			random_access_iterator operator+(difference_type n) const
+			{
+				return (itr_ptr + n);
+			}
+			random_access_iterator operator-(difference_type n) const
+			{
+				return (itr_ptr - n);
+			}
+			random_access_iterator operator+=(difference_type n)
+			{
+				itr_ptr += n;
+				return *this;
+			}
+			random_access_iterator operator-=(difference_type n)
+			{
+				itr_ptr -= n;
+				return *this;
+			}
+			reference operator[](difference_type n) const
+			{
+				return *(itr_ptr + n);
+			}
+			reference operator*() const
+			{
+				return *itr_ptr;
+			}
+			pointer operator->() const
+			{
+				return itr_ptr;
+			}
+			operator random_access_iterator<const value_type>() const
+			{
+				return random_access_iterator<const value_type>(itr_ptr);
+			}
 
 		protected:
 			pointer itr_ptr; 
-	}
+	};
 
 }
